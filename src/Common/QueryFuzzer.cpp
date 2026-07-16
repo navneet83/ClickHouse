db82598f997f141df1b075dc0930a2e37b36540e
Pervakov Grigorii <pervakov.grigory@gmail.com>
2026-07-16 15:47:11 +0000

    Support query parameters in user and role identifiers

--- src/Common/QueryFuzzer.cpp
+++ src/Common/QueryFuzzer.cpp
@@ -8637,7 +8637,7 @@ void QueryFuzzer::collectFuzzInfoRecurse(ASTPtr ast)
     {
         addColumnLike(ast);
     }
-    else if (typeid_cast<ASTIdentifier *>(ast.get()))
+    else if (const auto * identifier = typeid_cast<ASTIdentifier *>(ast.get()); identifier && !identifier->isParam())
     {
         addColumnLike(ast);
     }
